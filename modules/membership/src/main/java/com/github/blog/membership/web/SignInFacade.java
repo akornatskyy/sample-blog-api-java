3d0bca330c01ee78b0c850965375f59112c72b84
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2016-08-19 20:25:37 +0300

    Updated signin facade to use user service.

--- modules/membership/src/main/java/com/github/blog/membership/web/SignInFacade.java
+++ modules/membership/src/main/java/com/github/blog/membership/web/SignInFacade.java
@@ -1,13 +1,27 @@
 package com.github.blog.membership.web;
 
+import com.github.blog.membership.service.UserService;
 import com.github.blog.membership.web.models.SignInRequest;
 import com.github.blog.membership.web.models.SignInResponse;
 
 public final class SignInFacade {
+
+  private final UserService userService;
+
+  public SignInFacade(UserService userService) {
+    this.userService = userService;
+  }
+
   /**
    * Authenticates user request.
    */
   public SignInResponse authenticate(SignInRequest request) {
+    if (!this.userService.authenticate(
+        request.getUsername(),
+        request.getPassword())) {
+      return null;
+    }
+
     SignInResponse response = new SignInResponse();
     response.setUsername(request.getUsername());
     return response;
