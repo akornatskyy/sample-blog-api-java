7179a2e1d6908f193e590561f505417e59dc335a
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2016-08-20 11:59:39 +0300

    Updated config to use error state and user service.

--- modules/app/src/main/java/com/github/blog/api/Config.java
+++ modules/app/src/main/java/com/github/blog/api/Config.java
@@ -1,6 +1,9 @@
 package com.github.blog.api;
 
+import com.github.blog.membership.service.UserService;
+import com.github.blog.membership.service.UserServiceBridge;
 import com.github.blog.membership.web.SignInFacade;
+import com.github.blog.shared.service.ErrorState;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -9,8 +12,20 @@ import org.springframework.context.annotation.Scope;
 public class Config {
 
   @Bean
+  @Scope("request")
+  public ErrorState errorState() {
+    return new ErrorState();
+  }
+
+  @Bean
   @Scope("prototype")
-  public SignInFacade signInFacade() {
-    return new SignInFacade();
+  public UserService userService(ErrorState errorState) {
+    return new UserServiceBridge(errorState);
+  }
+
+  @Bean
+  @Scope("prototype")
+  public SignInFacade signInFacade(UserService userService) {
+    return new SignInFacade(userService);
   }
 }
