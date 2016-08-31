9ba6b31ca6115dfa97be219f7fa8e624ba5f6e80
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2016-08-31 20:42:46 +0300

    Updated user service bridge to use properties for messages.

--- modules/membership/src/main/java/com/github/blog/membership/service/UserServiceBridge.java
+++ modules/membership/src/main/java/com/github/blog/membership/service/UserServiceBridge.java
@@ -1,8 +1,36 @@
 package com.github.blog.membership.service;
 
 import com.github.blog.shared.service.ErrorState;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public final class UserServiceBridge implements UserService {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(
+      UserServiceBridge.class);
+  private static final Properties messages = new Properties();
+
+  static {
+    InputStream is = UserServiceBridge.class.getClassLoader()
+        .getResourceAsStream("membership-messages.properties");
+    if (is != null) {
+      try {
+        messages.load(is);
+      } catch (IOException e) {
+        LOGGER.warn("Failed to load messages.", e);
+      } finally {
+        try {
+          is.close();
+        } catch (IOException e) {
+          LOGGER.warn("Failed to close messages input stream.", e);
+        }
+      }
+    }
+  }
 
   private final ErrorState errorState;
 
@@ -13,7 +41,7 @@ public final class UserServiceBridge implements UserService {
   @Override
   public boolean authenticate(String username, String password) {
     if (!"demo".equalsIgnoreCase(username)) {
-      this.errorState.addError("signInFailed");
+      this.errorState.addError(messages.getProperty("signInFailed"));
       return false;
     }
 
