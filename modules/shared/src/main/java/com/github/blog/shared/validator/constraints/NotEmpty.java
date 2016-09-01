6c2381aff723d847d61478f6f3de3a0e02c2bb0d
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2016-09-01 20:47:32 +0300

    Reformat pom files.

--- modules/shared/src/main/java/com/github/blog/shared/validator/constraints/NotEmpty.java
+++ modules/shared/src/main/java/com/github/blog/shared/validator/constraints/NotEmpty.java
@@ -1,22 +1,21 @@
 package com.github.blog.shared.validator.constraints;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-
 import javax.validation.Constraint;
 import javax.validation.Payload;
 import javax.validation.ReportAsSingleViolation;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @NotNull
 @Size(min = 1)
 @Constraint(validatedBy = {})
 @ReportAsSingleViolation
 @Target({ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE,
-    ElementType.CONSTRUCTOR, ElementType.PARAMETER})
+         ElementType.CONSTRUCTOR, ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface NotEmpty {
   /**
@@ -39,7 +38,7 @@ public @interface NotEmpty {
    * List of annotations.
    */
   @Target({ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE,
-      ElementType.CONSTRUCTOR, ElementType.PARAMETER})
+           ElementType.CONSTRUCTOR, ElementType.PARAMETER})
   @Retention(RetentionPolicy.RUNTIME)
   @interface List {
     NotEmpty[] value();
