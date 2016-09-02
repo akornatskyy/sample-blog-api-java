0d6e5ed35d9254c9b55d06eaa1b9fa9457985918
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2016-09-02 20:29:59 +0300

    Fixed import order.

--- modules/shared/src/main/java/com/github/blog/shared/validator/constraints/NotEmpty.java
+++ modules/shared/src/main/java/com/github/blog/shared/validator/constraints/NotEmpty.java
@@ -1,14 +1,14 @@
 package com.github.blog.shared.validator.constraints;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import javax.validation.Constraint;
 import javax.validation.Payload;
 import javax.validation.ReportAsSingleViolation;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
 
 @NotNull
 @Size(min = 1)
