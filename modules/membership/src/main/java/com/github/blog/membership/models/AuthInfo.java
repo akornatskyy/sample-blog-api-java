b0a6bdb1df65cc03f1ca4def10cdc756f57a322c
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2016-09-05 20:28:42 +0300

    Introduced auth info model.

---
+++ modules/membership/src/main/java/com/github/blog/membership/models/AuthInfo.java
@@ -0,0 +1,31 @@
+package com.github.blog.membership.models;
+
+public final class AuthInfo {
+  private String userId;
+  private String password;
+  private boolean isLocked;
+
+  public String getUserId() {
+    return userId;
+  }
+
+  public void setUserId(String userId) {
+    this.userId = userId;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public boolean isLocked() {
+    return isLocked;
+  }
+
+  public void setLocked(boolean locked) {
+    isLocked = locked;
+  }
+}
