d5fc75ab84cb9e13422d5d6401785a0e2fcf9a88
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2016-09-13 20:35:46 +0300

    Refactoring user bridge message load.

--- modules/membership/src/main/java/com/github/blog/membership/service/UserServiceBridge.java
+++ modules/membership/src/main/java/com/github/blog/membership/service/UserServiceBridge.java
@@ -3,8 +3,6 @@ package com.github.blog.membership.service;
 import com.github.blog.membership.models.AuthInfo;
 import com.github.blog.membership.repository.UserRepository;
 import com.github.blog.shared.service.ErrorState;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -12,25 +10,14 @@ import java.util.Properties;
 
 public final class UserServiceBridge implements UserService {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(
-      UserServiceBridge.class);
   private static final Properties messages = new Properties();
 
   static {
-    InputStream is = UserServiceBridge.class.getClassLoader()
-        .getResourceAsStream("membership-messages.properties");
-    if (is != null) {
-      try {
-        messages.load(is);
-      } catch (IOException e) {
-        LOGGER.warn("Failed to load messages.", e);
-      } finally {
-        try {
-          is.close();
-        } catch (IOException e) {
-          LOGGER.warn("Failed to close messages input stream.", e);
-        }
-      }
+    try (InputStream is = Object.class.getResourceAsStream(
+        "/membership-messages.properties")) {
+      messages.load(is);
+    } catch (IOException ex) {
+      throw new IllegalStateException("Failed to load messages.");
     }
   }
 
