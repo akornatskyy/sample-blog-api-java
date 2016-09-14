798967ac1042d715716d996cdc06245747696683
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2016-09-14 20:15:18 +0300

    Added logger to signin facade.

--- modules/membership/src/main/java/com/github/blog/membership/web/SignInFacade.java
+++ modules/membership/src/main/java/com/github/blog/membership/web/SignInFacade.java
@@ -3,8 +3,13 @@ package com.github.blog.membership.web;
 import com.github.blog.membership.service.UserService;
 import com.github.blog.membership.web.models.SignInRequest;
 import com.github.blog.membership.web.models.SignInResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public final class SignInFacade {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(
+      SignInFacade.class);
 
   private final UserService userService;
 
@@ -16,10 +21,24 @@ public final class SignInFacade {
    * Authenticates user request.
    */
   public SignInResponse authenticate(SignInRequest request) {
+
+    if (LOGGER.isInfoEnabled()) {
+      LOGGER.info("authenticating {}", request.getUsername());
+    }
+
     if (!this.userService.authenticate(
         request.getUsername(),
         request.getPassword())) {
+
+      if (LOGGER.isWarnEnabled()) {
+        LOGGER.warn("authentication failed for {}", request.getUsername());
+      }
+
       return null;
+    }
+
+    if (LOGGER.isInfoEnabled()) {
+      LOGGER.info("authenticated {}", request.getUsername());
     }
 
     SignInResponse response = new SignInResponse();
