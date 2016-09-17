45a5bf682d19d40f4bf01e44f7f478380145675f
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2016-09-17 20:52:44 +0300

    Introduced signin facade test cases.

---
+++ modules/membership/src/test/java/com/github/blog/membership/web/SignInFacadeTest.java
@@ -0,0 +1,45 @@
+package com.github.blog.membership.web;
+
+import com.github.blog.membership.service.UserService;
+import com.github.blog.membership.web.models.SignInRequest;
+import com.github.blog.membership.web.models.SignInResponse;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class SignInFacadeTest {
+
+  @Mock
+  private UserService mockUserService;
+
+  private SignInRequest request;
+  private SignInFacade signInFacade;
+
+  @BeforeMethod
+  public void setUp() {
+    MockitoAnnotations.initMocks(this);
+    request = new SignInRequest();
+    request.setUsername("demo");
+    request.setPassword("password");
+    signInFacade = new SignInFacade(mockUserService);
+  }
+
+  @AfterMethod
+  public void tearDown() {
+    Mockito.verify(mockUserService).authenticate(
+        request.getUsername(), request.getPassword());
+  }
+
+  @Test
+  public void testAuthenticateFailed() {
+    SignInResponse response = signInFacade.authenticate(request);
+
+    Assert.assertNull(response);
+    Mockito.verify(mockUserService).authenticate(
+        request.getUsername(), request.getPassword());
+  }
+}
