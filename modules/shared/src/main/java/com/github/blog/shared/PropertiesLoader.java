824e260491f9f1e16e8524cb4c5bf8faf93ed0a8
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2016-09-24 20:57:15 +0300

    Introduced properties loader.

---
+++ modules/shared/src/main/java/com/github/blog/shared/PropertiesLoader.java
@@ -0,0 +1,27 @@
+package com.github.blog.shared;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public final class PropertiesLoader {
+  private PropertiesLoader() {
+  }
+
+  /**
+   * Uses class loader to load properties from resource stream.
+   */
+  public static Properties fromResource(String name) {
+    Properties properties = new Properties();
+    try (InputStream in = PropertiesLoader.class.getClassLoader()
+        .getResourceAsStream(name + ".properties")) {
+      properties.load(in);
+    } catch (IOException ex) {
+      throw new IllegalStateException(
+          String.format("Unable to load %s.properties file.", name));
+    }
+
+    return properties;
+  }
+}
