bbad8e1ef41dfefb05babe11f67c5e99a8175a76
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2016-10-16 20:11:19 +0300

    Added create account to user repository.

--- modules/membership/src/main/java/com/github/blog/membership/repository/UserRepository.java
+++ modules/membership/src/main/java/com/github/blog/membership/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package com.github.blog.membership.repository;
 
 import com.github.blog.membership.models.AuthInfo;
+import com.github.blog.membership.models.Registration;
 
 public interface UserRepository {
   AuthInfo findAuthInfo(String username);
 
   boolean hasAccount(String username);
+
+  boolean createAccount(Registration registration);
 }
