e876c60ae4c1a00606cfdc894b66a61edb0521b1
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2016-10-18 20:14:41 +0300

    Added create account to user service contract.

--- modules/membership/src/main/java/com/github/blog/membership/service/UserService.java
+++ modules/membership/src/main/java/com/github/blog/membership/service/UserService.java
@@ -1,5 +1,9 @@
 package com.github.blog.membership.service;
 
+import com.github.blog.membership.models.Registration;
+
 public interface UserService {
   boolean authenticate(String username, String password);
+
+  boolean createAccount(Registration registration);
 }
