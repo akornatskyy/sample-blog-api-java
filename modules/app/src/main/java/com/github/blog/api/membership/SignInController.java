ef496eb78b2ef1422e07ce31edcd4b5c35145738
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2016-10-21 20:43:29 +0300

    Eliminated unnecessary constructor annotation in signin controller.

--- modules/app/src/main/java/com/github/blog/api/membership/SignInController.java
+++ modules/app/src/main/java/com/github/blog/api/membership/SignInController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import javax.inject.Inject;
 import javax.validation.Valid;
 
 @Controller
@@ -19,7 +18,6 @@ public final class SignInController {
 
   private final SignInFacade signInFacade;
 
-  @Inject
   public SignInController(final SignInFacade signInFacade) {
     this.signInFacade = signInFacade;
   }
