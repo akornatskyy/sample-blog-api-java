d80f3551c37c559c4c6a28ce0145d8906e4a352d
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2016-10-22 20:44:36 +0300

    Override extend message converters in spring config to customize default behavior of json object mapper.

--- modules/app/src/main/java/com/github/blog/api/Config.java
+++ modules/app/src/main/java/com/github/blog/api/Config.java
@@ -1,5 +1,9 @@
 package com.github.blog.api;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.github.blog.membership.repository.UserRepository;
 import com.github.blog.membership.repository.mock.UserRepositoryImpl;
 import com.github.blog.membership.service.UserService;
@@ -9,9 +13,14 @@ import com.github.blog.shared.service.ErrorState;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
 
 @Configuration
-public class Config {
+public class Config extends WebMvcConfigurerAdapter {
 
   @Bean
   @Scope("request")
@@ -36,5 +45,16 @@ public class Config {
   @Scope("prototype")
   public SignInFacade signInFacade(UserService userService) {
     return new SignInFacade(userService);
+  }
+
+  @Override
+  public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+    converters.removeIf(
+        c -> c.getClass().equals(MappingJackson2HttpMessageConverter.class));
+    ObjectMapper mapper = new ObjectMapper();
+    mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+    mapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+    mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    converters.add(0, new MappingJackson2HttpMessageConverter(mapper));
   }
 }
