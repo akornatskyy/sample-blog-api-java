ec7f0de5a2bbdb7f6852328eb68dd1b43e359192
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2017-01-13 20:40:18 +0200

    Refactored api request handler with routing.

--- modules/aws/src/main/java/com/github/blog/aws/lambda/ApiRequestHandler.java
+++ modules/aws/src/main/java/com/github/blog/aws/lambda/ApiRequestHandler.java
@@ -4,11 +4,11 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.blog.shared.service.ErrorState;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.util.HashMap;
 
 public final class ApiRequestHandler
     implements RequestHandler<HttpRequest, HttpResponse> {
@@ -16,11 +16,19 @@ public final class ApiRequestHandler
   private static final Logger LOGGER = LoggerFactory.getLogger(
       ApiRequestHandler.class);
 
+  private static final int SC_BAD_REQUEST = 400;
+
   private final ObjectMapper mapper;
+  private final FactoryProvider factoryProvider;
 
   public ApiRequestHandler() {
+    this(new FactoryProvider());
+  }
+
+  ApiRequestHandler(FactoryProvider factoryProvider) {
     mapper = new ObjectMapper();
     mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    this.factoryProvider = factoryProvider;
   }
 
   @Override
@@ -29,23 +37,64 @@ public final class ApiRequestHandler
       LOGGER.info("handling request");
     }
 
-    HttpResponse response = new HttpResponse();
+    String routeName = Router.match(request);
+    if (routeName == null) {
+      return HttpResponse.NOT_FOUND;
+    }
 
+    request.setRouteName(routeName);
+
+    HttpResponse response;
     try {
-      response.setBody(mapper.writeValueAsString(new HashMap<String, Object>() {
-        {
-          put("message", "Hello World!");
-          put("request", request);
-        }
-      }));
+      response = route(request);
+    } catch (Exception ex) {
+      LOGGER.error("unhandled error", ex);
+      return HttpResponse.INTERNAL_SERVER_ERROR;
+    }
+
+    return response;
+  }
+
+  private HttpResponse route(HttpRequest request) throws IOException {
+    ErrorState errorState = new ErrorState();
+    Factory factory = factoryProvider.from(request.getStageVariables());
+    Object result;
+    switch (request.getRouteName()) {
+      case RouteNames.WELCOME:
+        result = factory.createWelcomeFacade()
+            .process(request);
+        break;
+      default:
+        throw new IllegalStateException(
+            "Unknown route name: " + request.getRouteName());
+    }
+
+    HttpResponse response = new HttpResponse();
+    if (result == null) {
+      response.setStatusCode(SC_BAD_REQUEST);
+      result = errorState.getErrors();
+    }
+
+    response.setBody(render(result));
+    return response;
+  }
+
+  private String render(Object value) {
+    if (LOGGER.isInfoEnabled()) {
+      LOGGER.info("rendering response");
+    }
+
+    String content;
+    try {
+      content = mapper.writeValueAsString(value);
     } catch (IOException e) {
       throw new IllegalStateException(e);
     }
 
     if (LOGGER.isInfoEnabled()) {
-      LOGGER.info("finished");
+      LOGGER.info(String.format("finished %.3f KB", content.length() / 1024F));
     }
 
-    return response;
+    return content;
   }
 }
