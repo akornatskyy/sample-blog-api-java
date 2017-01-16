539f8c858a98ab71785751dd1edcc53139ba3e9b
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2017-01-16 20:13:52 +0200

    Added signin facade to factory.

--- modules/aws/src/main/java/com/github/blog/aws/lambda/Factory.java
+++ modules/aws/src/main/java/com/github/blog/aws/lambda/Factory.java
@@ -1,12 +1,31 @@
 package com.github.blog.aws.lambda;
 
+import com.github.blog.membership.repository.UserRepository;
+import com.github.blog.membership.repository.mock.UserRepositoryImpl;
+import com.github.blog.membership.service.UserService;
+import com.github.blog.membership.service.UserServiceBridge;
+import com.github.blog.membership.web.SignInFacade;
+import com.github.blog.shared.service.ErrorState;
+
 import java.util.Properties;
 
-public class Factory {
+class Factory {
   public Factory(Properties properties) {
   }
 
   public WelcomeFacade createWelcomeFacade() {
     return new WelcomeFacade();
+  }
+
+  public SignInFacade createSignInFacade(ErrorState errorState) {
+    return new SignInFacade(userService(errorState));
+  }
+
+  private UserService userService(ErrorState errorState) {
+    return new UserServiceBridge(errorState, userRepository());
+  }
+
+  private UserRepository userRepository() {
+    return new UserRepositoryImpl();
   }
 }
