a3db5ee3615984c1c25d9ededaf9ccf48ae81a67
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2017-01-31 20:09:42 +0200

    Added route name for signup.

--- modules/aws/src/main/java/com/github/blog/aws/lambda/RouteNames.java
+++ modules/aws/src/main/java/com/github/blog/aws/lambda/RouteNames.java
@@ -3,6 +3,7 @@ package com.github.blog.aws.lambda;
 final class RouteNames {
   public static final String WELCOME = "welcome";
   public static final String SIGNIN = "signin";
+  public static final String SIGNUP = "signup";
 
   private RouteNames() {
   }
