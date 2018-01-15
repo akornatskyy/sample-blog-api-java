2662d1ea7e2aeac6cf92f70b2018c352c2fde0d2
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2018-01-15 20:10:05 +0200

    Added signin facade functional test case: request cause validation errors.

--- modules/membership/src/test/java/com/github/blog/membership/web/SignInFacadeFunctionalTest.java
+++ modules/membership/src/test/java/com/github/blog/membership/web/SignInFacadeFunctionalTest.java
@@ -18,7 +18,8 @@ public final class SignInFacadeFunctionalTest {
   public Object[][] samples() {
     return new Object[][] {
         {1, "a null request causes validation errors"},
-        {2, "an empty request causes validation errors"}
+        {2, "an empty request causes validation errors"},
+        {3, "request cause validation errors"}
     };
   }
 
