0399f921859a71e2a72b0e6d208cd6831ee5b386
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2019-01-16 20:02:47 +0200

    Migrated to spring boot 2.

--- sample-blog-app/src/main/java/com/github/blog/api/Config.java
+++ sample-blog-app/src/main/java/com/github/blog/api/Config.java
@@ -10,12 +10,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
-public class Config extends WebMvcConfigurerAdapter {
+public class Config implements WebMvcConfigurer {
 
   @Bean
   public Factory factory(Environment environment) {
