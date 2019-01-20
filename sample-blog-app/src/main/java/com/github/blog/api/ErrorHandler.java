75782dc5ea1f03a58d908cd2f707c6fbd57719ae
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2019-01-20 20:18:32 +0200

    Combined error handlers into a single class.

--- sample-blog-app/src/main/java/com/github/blog/api/ErrorHandler.java
+++ sample-blog-app/src/main/java/com/github/blog/api/ErrorHandler.java
@@ -1,15 +1,34 @@
-package com.github.blog.api.interceptors;
+package com.github.blog.api;
 
 import com.github.blog.shared.service.ErrorState;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
-public final class HttpMessageNotReadableResponseBodyAdvice {
+public final class ErrorHandler {
+
+  /**
+   * Handles validation errors and translates to response.
+   */
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @ResponseBody
+  public ErrorState handleMethodArgumentNotValidException(
+      MethodArgumentNotValidException ex) {
+
+    ErrorState errorState = new ErrorState();
+    for (FieldError fe : ex.getBindingResult().getFieldErrors()) {
+      errorState.addError(fe.getField(), fe.getDefaultMessage());
+    }
+
+    return errorState;
+  }
 
   /**
    * Handles message not readable response.
