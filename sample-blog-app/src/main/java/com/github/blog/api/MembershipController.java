e01904059867f89759dc9c78fbf65a1ba2b0ec11
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2019-01-21 20:22:30 +0200

    Moved signin and signup into membership controller.

--- sample-blog-app/src/main/java/com/github/blog/api/MembershipController.java
+++ sample-blog-app/src/main/java/com/github/blog/api/MembershipController.java
@@ -1,6 +1,9 @@
-package com.github.blog.api.membership;
+package com.github.blog.api;
 
 import com.github.blog.membership.Factory;
+import com.github.blog.membership.core.SignInFacade;
+import com.github.blog.membership.core.SignInRequest;
+import com.github.blog.membership.core.SignInResponse;
 import com.github.blog.membership.core.SignUpFacade;
 import com.github.blog.membership.core.SignUpRequest;
 import com.github.blog.membership.core.SignUpResponse;
@@ -13,12 +16,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public final class SignUpController {
+public class MembershipController {
 
   private final Factory factory;
 
-  public SignUpController(final Factory factory) {
+  public MembershipController(final Factory factory) {
     this.factory = factory;
+  }
+
+  /**
+   * Responds to HTTP POST signin request.
+   */
+  @RequestMapping(value = "/signin", method = RequestMethod.POST)
+  public ResponseEntity<?> post(@RequestBody SignInRequest request) {
+
+    ErrorState errorState = new ErrorState();
+    SignInFacade facade = factory.createSignInFacade(errorState);
+
+    SignInResponse response = facade.authenticate(request);
+
+    if (response == SignInResponse.ERROR) {
+      return new ResponseEntity<>(errorState, HttpStatus.BAD_REQUEST);
+    }
+
+    return new ResponseEntity<>(response, HttpStatus.OK);
   }
 
   /**
