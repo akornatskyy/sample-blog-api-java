aaeae12ec879cfc87df2d01761a486358072dd1b
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2019-01-24 20:07:17 +0200

    Added unit tests for membership controller signup.

--- sample-blog-app/src/test/java/com/github/blog/api/MembershipControllerTest.java
+++ sample-blog-app/src/test/java/com/github/blog/api/MembershipControllerTest.java
@@ -4,6 +4,9 @@ import com.github.blog.membership.Factory;
 import com.github.blog.membership.core.SignInFacade;
 import com.github.blog.membership.core.SignInRequest;
 import com.github.blog.membership.core.SignInResponse;
+import com.github.blog.membership.core.SignUpFacade;
+import com.github.blog.membership.core.SignUpRequest;
+import com.github.blog.membership.core.SignUpResponse;
 import com.github.blog.shared.service.ErrorState;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
@@ -47,6 +50,39 @@ public class MembershipControllerTest {
         .thenReturn(mockSignInFacade);
 
     ResponseEntity<?> entity = controller.signIn(request);
+
+    Assert.assertNotNull(entity);
+    Assert.assertEquals(entity.getStatusCode(), HttpStatus.BAD_REQUEST);
+    Assert.assertEquals(entity.getBody(), errorStateCaptor.getValue());
+  }
+
+  @Test
+  public void testSignUp() {
+    SignUpRequest request = new SignUpRequest();
+    SignUpResponse response = new SignUpResponse();
+    SignUpFacade mockSignUpFacade = Mockito.mock(SignUpFacade.class);
+    Mockito.when(mockSignUpFacade.createAccount(request))
+        .thenReturn(response);
+    Mockito.when(mockFactory.createSignUpFacade(Mockito.any()))
+        .thenReturn(mockSignUpFacade);
+
+    ResponseEntity<?> entity = controller.signUp(request);
+
+    Assert.assertNotNull(entity);
+    Assert.assertEquals(entity.getStatusCode(), HttpStatus.OK);
+    Assert.assertEquals(entity.getBody(), response);
+  }
+
+  @Test
+  public void testSignUpError() {
+    SignUpRequest request = new SignUpRequest();
+    SignUpFacade mockSignUpFacade = Mockito.mock(SignUpFacade.class);
+    Mockito.when(mockSignUpFacade.createAccount(request))
+        .thenReturn(SignUpResponse.ERROR);
+    Mockito.when(mockFactory.createSignUpFacade(errorStateCaptor.capture()))
+        .thenReturn(mockSignUpFacade);
+
+    ResponseEntity<?> entity = controller.signUp(request);
 
     Assert.assertNotNull(entity);
     Assert.assertEquals(entity.getStatusCode(), HttpStatus.BAD_REQUEST);
