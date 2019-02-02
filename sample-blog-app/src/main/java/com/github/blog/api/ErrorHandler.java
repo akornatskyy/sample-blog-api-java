ddf8a432114275235eb5435e0f620ac3269bfa78
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2019-02-02 20:17:33 +0200

    Added resource not found error handler.

--- sample-blog-app/src/main/java/com/github/blog/api/ErrorHandler.java
+++ sample-blog-app/src/main/java/com/github/blog/api/ErrorHandler.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @ControllerAdvice
 final class ErrorHandler {
@@ -38,8 +39,28 @@ final class ErrorHandler {
   @ResponseBody
   public ErrorState handleMethodArgumentNotValidException(
       HttpMessageNotReadableException ex) {
+
     ErrorState errorState = new ErrorState();
     errorState.addError("The HTTP message is not readable.");
+    return errorState;
+  }
+
+  /**
+   * Handles resource not found response.
+   */
+  @ExceptionHandler(NoHandlerFoundException.class)
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  @ResponseBody
+  public ErrorState handleNoHandlerFoundException(
+      NoHandlerFoundException ex) {
+
+    ErrorState errorState = new ErrorState();
+    errorState.addError(
+        "Oops! Code 404. Sorry, we can't find that resource. "
+        + "Unfortunately the resource you are looking for may have been "
+        + "removed, had its name changed, under construction or is temporarily "
+        + "unavailable. Try checking the web address for typos, please. "
+        + "We apologize for the inconvenience.");
     return errorState;
   }
 }
